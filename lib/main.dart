1f3e675408a257cedfbaa314ff13d4e27cec5373
sarthakjoshi <sarthakdjoshi20@gmail.com>
2024-02-22 15:12:51 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:ui';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:pratice/Splash.dart';
-
 import 'firebase_options.dart';
 
 void main() async {
